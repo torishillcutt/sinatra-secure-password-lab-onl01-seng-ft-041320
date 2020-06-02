e39cf51d450f69ac305133c59b072134620e345c
github username <github email address>
2020-06-01 23:13:28 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 require "./config/environment"
 require "./app/models/user"
+require "pry"
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -17,13 +18,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(username: params[:username], password: params[:password])
+    #binding.pry
+    if user.save  && user.username != ""
+      redirect "/login"
+    else
+      redirect "/failure"
+    end
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
+    if logged_in?
+    @user = current_user
     erb :account
+    else
+      redirect "/login"
+    end
   end
 
 
@@ -32,7 +42,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
   end
 
   get "/failure" do
---
+++ db/migrate/20200602024608_create_users_table.rb
@@ -0,0 +1,9 @@
+class CreateUsersTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.float :balance, :default => 0
+    end 
+  end
+end
